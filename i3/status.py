7bad7870d86add22af64a0dde3eca39a0d981862
Lauri Gustafsson <luutifa@gmail.com>
2017-07-04 18:27:23 +0300

    i3pystatus hostname detection

--- i3/status.py
+++ i3/status.py
@@ -5,6 +5,10 @@ status = Status()
 # Colors
 cGreen = "#A0D0A0"
 
+# Host
+import socket
+cHostname = socket.gethostname()
+
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
@@ -31,9 +35,10 @@ status.register("mem",
 status.register("load")
 
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",
-    file="/sys/class/thermal/thermal_zone3/temp",)
+if cHostname == "mu":
+    status.register("temp",
+        format="{temp:.0f}°C",
+        file="/sys/class/thermal/thermal_zone3/temp",)
 
 # The battery monitor has many formatting options, see README for details
 
@@ -46,16 +51,17 @@ status.register("temp",
 # goes below 5 percent while discharging. The block will also color RED.
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
-status.register("battery",
-    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-    alert=False,
-    full_color=cGreen,
-    charging_color=cGreen,
-    status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
+if cHostname == "kappa":
+    status.register("battery",
+        format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+        alert=False,
+        full_color=cGreen,
+        charging_color=cGreen,
+        status={
+            "DIS": "↓",
+            "CHR": "↑",
+            "FULL": "=",
+        },)
 
 # This would look like this:
 # Discharging 6h:51m
@@ -81,17 +87,19 @@ status.register("battery",
 # (defaults of format_down and color_down)
 #
 # Note: the network module requires PyPI package netifaces
-status.register("network",
-    interface="wired0",
-    format_up="{v4cidr}",
-    color_up=cGreen,
-    color_down="#D0A0A0")
+if cHostname == "mu":
+    status.register("network",
+        interface="wired0",
+        format_up="{v4cidr}",
+        color_up=cGreen,
+        color_down="#D0A0A0")
 
 # Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-    interface="wlp3s0",
-    color_up=cGreen,
-    format_up="{essid} {quality:02.0f}% {v4cidr}",)
+if cHostname == "kappa":
+    status.register("network",
+        interface="wlp3s0",
+        color_up=cGreen,
+        format_up="{essid} {quality:02.0f}% {v4cidr}",)
 
 # Shows disk usage of /
 # Format:

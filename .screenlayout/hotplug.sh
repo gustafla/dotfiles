955520bb6c2f43f833840074ff33557b34d9ea47
Lauri Gustafsson <luutifa@gmail.com>
2018-03-19 23:04:05 +0200

    Screen hotplug script

---
+++ .screenlayout/hotplug.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+read -r DP_STATUS</sys/class/drm/card0/card0-DP-1/status
+
+if [ "connected" = "$DP_STATUS" ]; then
+  /bin/sh $(dirname "$0")/dp.sh
+else
+  /bin/sh $(dirname "$0")/none.sh
+fi
+
+# reset desktop background related stuff
+feh --bg-fill ~/.config/i3/bg
+killall -HUP urxvtd

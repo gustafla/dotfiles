01dae23541c0f2331f6a4da4d2a82af7bbe38848
Lauri Gustafsson <luutifa@gmail.com>
2018-05-08 14:44:17 +0300

    Remove GIMP rules and added battery wattage status

--- status.sh
+++ status.sh
@@ -2,6 +2,8 @@
 
 while [ 1 ]; do
     STR=""
+    BAT="`cat /sys/class/power_supply/BAT0/power_now`"
+    STR="$STR | `echo "scale=1;$BAT/1000000"|bc`W"
     STR="$STR | `cat /sys/class/power_supply/BAT0/capacity`%"
     STR="$STR | `date`"
     xsetroot -name "$STR"

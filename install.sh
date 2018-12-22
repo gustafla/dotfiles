0d6a6def9c8de31327514faf2827786062eaf409
Lauri Gustafsson <luutifa@jkry.org>
2018-12-22 14:04:36 +0200

    Add .xserverrc and switch to curl for vim-plug

--- install.sh
+++ install.sh
@@ -20,5 +20,4 @@ for f in $files; do
 done
 
 # install vim-plug
-mkdir -p ~/.local/share/nvim/site/autoload
-aria2c -d ~/.local/share/nvim/site/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

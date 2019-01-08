0b3f86aa41b73dd66f82d342351352fd2a5ad31b
Lauri Gustafsson <luutifa@jkry.org>
2019-01-08 13:34:00 +0200

    Don't report battery percentage when no batteries

--- status.sh
+++ status.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 while [ 1 ]; do
-	nbat=`find /sys/class/power_supply -maxdepth 1 -name "BAT*" | wc -l`
-	let bat=0
-	for d in /sys/class/power_supply/BAT*; do
-		let bat=$bat+`cat $d/capacity`
-	done
-	let bat=$bat/$nbat
-	
 	STR=""
-	STR="$STR | $bat%"
+
+	nbat=`find /sys/class/power_supply -maxdepth 1 -name "BAT*" | wc -l`
+	if [ $nbat -gt 0 ]; then
+		let bat=0
+		for d in /sys/class/power_supply/BAT*; do
+			let bat=$bat+`cat $d/capacity`
+		done
+		let bat=$bat/$nbat
+
+		STR="$STR | $bat%"
+	fi
+
 	STR="$STR | `date +%F\ %T`"
 	xsetroot -name "$STR"
 	sleep 10

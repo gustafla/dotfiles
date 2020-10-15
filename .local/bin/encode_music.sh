56999e51e86047c35c795087b6978c14f07ba2d1
Lauri Gustafsson <me@gustafla.space>
2020-10-16 02:46:25 +0300

    Optimize encode_music.sh

--- .local/bin/encode_music.sh
+++ .local/bin/encode_music.sh
@@ -8,45 +8,21 @@ bitrate="128K"
 src_dir="$1"
 dst_dir="$(pwd)/$2"
 
+# Work in source dir, clone dir structure into dst
 cd "$src_dir"
 find . -type d -exec mkdir -p "$dst_dir/{}" \;
-rsync -vrt \
-	--exclude='*.flac' \
-	--exclude='*.wav' \
-	--exclude='*.cue' \
-	--exclude='*.png' \
-	--exclude='*.jpg' \
-	--exclude='*.JPG' \
-	--exclude='*.jpeg' \
-	--exclude='*.tiff' \
-	--exclude='*.tif' \
-	--exclude='*.txt' \
-	--exclude='*.log' \
-	--exclude='*.accurip' \
-	--exclude='*.ffp' \
-	--exclude='*.m3u' \
-	--exclude='*.m3u8' \
-	--exclude='*.url' \
-	--exclude='*.nfo' \
-	--exclude='*.md5' \
-	--exclude='*.pdf' \
-	--exclude='*.mp4' \
-	--exclude='*.mkv' \
-	--exclude='*.avi' \
-	--exclude='*.7z' \
-	--exclude='*.tar*' \
-	--exclude='*.tgz' \
-	--exclude='*.zip' \
-	--exclude='*.rar' \
-	--exclude='*.it' \
-	--exclude='*.mod' \
-	--exclude='*.xm' \
-	--exclude='*.s3m' \
-	. "$dst_dir"
+
 echo Starting encoding
+
+# Strip pictures from lossy files
+find . -type f -regex '.*\.\(aac\|m4a\|mp3\|ogg\|opus\)' | \
+parallel ffmpeg -n -i {} -vn -codec:a copy "$dst_dir/{}"
+
+# Encode lossless files to opus
 find . -type f -regex '.*\.\(flac\|wav\)' | \
 parallel \
 ffmpeg -n -i {} \
+-vn \
 -codec:a libopus \
 -b:a $bitrate \
 -filter:a aresample=48000:\
@@ -56,4 +32,5 @@ dither_method=triangular \
 -ar 48000 \
 "$dst_dir/{.}.opus"
 
+# Send notification
 notify-send "$(basename $0) finished" "$(du -sh "$dst_dir")"

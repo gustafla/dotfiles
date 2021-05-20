ba63b307d6dc12189b943ad269f5c0896c2998f9
Lauri Gustafsson <me@gustafla.space>
2021-05-21 01:30:20 +0300

    Add gaussian blur to wallpaper script

--- .config/sway/bg.sh
+++ .config/sway/bg.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-BGPATH="$HOME/pictures/wallpapers"
+tmpprefix=$USER-bg
+rm -f ${TMPDIR:-/tmp}/$tmpprefix*
+bgpath="$HOME/pictures/wallpapers"
 mood=cold
 hour=$(date +%H)
 if [[ $hour -gt 17 ]] || [[ $hour -lt 7 ]]; then
     mood=warm
 fi
-background=$(ls "$BGPATH/$mood" | shuf -n 1)
-swaymsg output '*' bg "$BGPATH/$mood/$background" fill
+background=$(ls "$bgpath/$mood" | shuf -n 1)
+highestres=$(swaymsg -t get_outputs | jq -r '.[].current_mode | "\(.width * .height) \(.width)x\(.height)"' | sort -n -r -k 1 | head -n 1 | cut -d' ' -f 2)
+blurred=$(mktemp --tmpdir $tmpprefix-XXXXXXXXXXXX)
+magick "$bgpath/$mood/$background" -resize $highestres^ -gravity center -extent $highestres -gaussian-blur x2 $blurred
+swaymsg output '*' bg $blurred fill

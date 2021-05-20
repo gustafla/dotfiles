8a40b6271ed4a7b780598f538d638dc86ad4fd58
Lauri Gustafsson <me@gustafla.space>
2021-05-21 01:55:51 +0300

    Fix queries with passive outputs in bg.sh

--- .config/sway/bg.sh
+++ .config/sway/bg.sh
@@ -8,7 +8,7 @@ if [[ $hour -gt 17 ]] || [[ $hour -lt 7 ]]; then
     mood=warm
 fi
 background=$(ls "$bgpath/$mood" | shuf -n 1)
-highestres=$(swaymsg -t get_outputs | jq -r '.[].current_mode | "\(.width * .height) \(.width)x\(.height)"' | sort -n -r -k 1 | head -n 1 | cut -d' ' -f 2)
+highestres=$(swaymsg -t get_outputs | jq -r '.[].current_mode | select(. != null) | "\(.width * .height) \(.width)x\(.height)"' | sort -n -r -k 1 | head -n 1 | cut -d' ' -f 2)
 blurred=$(mktemp --tmpdir $tmpprefix-XXXXXXXXXXXX)
-magick "$bgpath/$mood/$background" -resize $highestres^ -gravity center -extent $highestres -gaussian-blur x2 $blurred
+magick "$bgpath/$mood/$background" -resize $highestres^ -gravity center -extent $highestres -gaussian-blur x3 $blurred
 swaymsg output '*' bg $blurred fill

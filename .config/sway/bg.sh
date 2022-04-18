4e65b342c46ba7a747304964741ff583b92c5c8a
Lauri Gustafsson <me@gustafla.space>
2022-04-18 17:31:19 +0300

    Speed up bg blur

--- .config/sway/bg.sh
+++ .config/sway/bg.sh
@@ -27,7 +27,7 @@ bgpath="$bgdir/$mood/$background"
 
 # Blur all pictures
 tmpfile=$(mktemp --tmpdir $tmpprefix-XXXXXXXXXXXX)
-magick "$bgpath" -blur x10 $tmpfile
+magick "$bgpath" -resize 10% -blur x40 -resize 1000% $tmpfile
 bgpath=$tmpfile
 
 # Set it

10b1728fd13136995567e5b0f3262764a4268bb7
Lauri Gustafsson <me@gustafla.space>
2023-03-23 14:39:08 +0200

    Make music encode script more aggressive

--- .local/bin/encode_music.sh
+++ .local/bin/encode_music.sh
@@ -4,26 +4,22 @@
     echo "Usage: $0 src_dir dst_dir" && \
     exit 1
 
-bitrate="128K"
+bitrate="64K"
 src_dir="$1"
 dst_dir="$PWD/$2"
 
 # Work in source dir
 cd "$src_dir"
 
-# Copy lossy files and directory structure
+# Copy opus files and directory structure
 rsync -rt --include '*/' \
---include '*.aac' \
---include '*.m4a' \
---include '*.mp3' \
---include '*.ogg' \
 --include '*.opus' \
 --exclude '*' . "$dst_dir"
 
 echo Starting encoding
 
-# Encode lossless files to opus
-find . -type f -regex '.*\.\(flac\|wav\)' | \
+# Encode files to opus
+find . -type f -regex '.*\.\(flac\|wav\|aac\|m4a\|mp3\|ogg\)' | \
 parallel \
 ffmpeg -n -i {} \
 -codec:a libopus \

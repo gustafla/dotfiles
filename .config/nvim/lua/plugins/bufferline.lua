e06f950847575cbedce7d1c8da6cc0016eb0faf5
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-06-27 14:41:11 +0300

    nvim: configure bufferline options

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,12 @@
 return {
     "akinsho/bufferline.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {},
+    opts = {
+        options = {
+            always_show_bufferline = false,
+            show_close_icon = false,
+            show_buffer_close_icons = false,
+            hover = { enabled = false },
+        },
+    },
 }

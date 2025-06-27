69f5530b193e8a0a72fc66a3fe52290c6cd32373
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-06-27 14:24:23 +0300

    nvim: Enable line numbers

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -25,6 +25,8 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.list = true
 vim.o.listchars = "tab:>·,trail:·"
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
